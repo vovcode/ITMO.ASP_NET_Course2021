cd62b6cdcd872752222230b677eb137d1a746f9c
vovcode <cromwell614@gmail.com>
2021-09-13 16:49:56 +0300

    Add project files.

---
+++ RSVP/Reg.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace RSVP
+{
+    public partial class Reg : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            {
+                GuestResponse rsvp = new GuestResponse(name.Text, email.Text, phone.Text, CheckBoxYN.Checked);
+                ResponseRepository.GetRepository().AddResponse(rsvp);
+                if (rsvp.WillAttend.HasValue && rsvp.WillAttend.Value)
+                {
+                    Response.Redirect("seeyouthere.html");
+                }
+                else
+                {
+                    Response.Redirect("sorryyoucantcome.html"); 
+                }
+            }
+        }
+    }
+}
